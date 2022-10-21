79064ae90c253bc48ef989e8c97296c8e7333b0c
Muhammad Rizal <rhy159753@gmail.com>
2022-10-21 17:06:50 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from 'react'
 
-function App() {
+export default function App() {
+  const [abjad, setAbjad] = useState('abcdefghijklmnopqrstuvwxyz')
+  // Ref Hooks
+  const plaintext = useRef()
+  const keyPlainText = useRef()
+  const ciphertext = useRef()
+  const keyCiphertext = useRef()
+
+  // state
+  const [cipherState, setCipherState] = useState(null);
+  const [plainState, setPlainState] = useState(null);
+
+  // Button Action
+  const encrypt = () => {
+    const plain = plaintext.current.value
+    const key = keyPlainText.current.value
+    if (plain !== '' && key !== '') {
+      runEncrypt(plain, key)
+    } else {
+      console.log('nda boleh kosong')
+    }
+  }
+
+  const decrypt = () => {
+    const cipher = ciphertext.current.value
+    const key = keyCiphertext.current.value
+    if (cipher !== '' && key !== '') {
+      runDecrypt(cipher, key)
+    } else {
+      console.log('nda boleh kosong')
+    }
+  }
+
+  // function
+  const runEncrypt = (plaintext, key) => {
+    const subPlainText = plaintext.split(" ")
+    let textArray = []
+    subPlainText.forEach(karakter => {
+      let text = ""
+      for (let index = 0; index < karakter.length; index++) {
+        const element = karakter[index];
+        const cipher = (abjad.indexOf(element) + parseInt(key)) % 26
+        const cipherOutput = abjad[cipher]
+        text +=  cipherOutput
+      }
+      textArray.push(text)
+    });
+    let outputStream = textArray.toString().replaceAll(",", " ")
+    setCipherState(outputStream)
+  }
+
+  const runDecrypt = (ciphertext, key) => {
+    const subCipherText = ciphertext.split(" ")
+    let textArray = []
+    subCipherText.forEach(karakter => {
+      let text = ""
+      for (let index = 0; index < karakter.length; index++) {
+        const element = karakter[index];
+        const cipher = (abjad.indexOf(element) - parseInt(key)) % 26
+        console.log(cipher)
+        const cipherOutput = abjad[cipher]
+        text +=  cipherOutput
+      }
+      textArray.push(text)
+    });
+    let outputStream = textArray.toString().replaceAll(",", " ")
+    setPlainState(outputStream)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-slate-800 w-full h-screen text-white'>
+      <h1 className='text-center font-bold text-3xl py-4'>Caesar Cipher with JavaScript</h1>
+      <div className='flex flex-row justify-items-center mt-12'>
+        <div className='flex-1 mx-8'>
+          <div>
+            <label className='block text-sm font-medium'>Plain Text</label>
+            <textarea ref={plaintext} placeholder='Input you plaintext ....' className='w-full h-64 mt-2 p-2 border text-slate-900 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500' />
+          </div>
+          <div className='mt-4'>
+            <label className='block text-sm font-medium'>Key (1 - 26)</label>
+            <input ref={keyPlainText} type='number' max={26} min={2} placeholder='Input you Key ....' className='w-full mt-2 p-2 border text-slate-900 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500' />
+          </div>
+          <div className='mt-4'>
+            <button onClick={() => encrypt()} className='w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded'>Encrypt</button>
+          </div>
+          <div className='mt-4'>
+            <p>Cipher text : {cipherState == null ? '' : <span className='bg-slate-500 px-1'>{cipherState}</span>}</p>
+          </div>
+        </div>
+        <div className='flex-1 mx-8'>
+          <div>
+            <label className='block text-sm font-medium'>Cipher Text</label>
+            <textarea ref={ciphertext} placeholder='Input you plaintext ....' className='w-full h-64 mt-2 p-2 border text-slate-900 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500' />
+          </div>
+          <div className='mt-4'>
+            <label className='block text-sm font-medium'>Key (1 - 26)</label>
+            <input ref={keyCiphertext} type='number' max={26} min={2} placeholder='Input you Key ....' className='w-full mt-2 p-2 border text-slate-900 border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500' />
+          </div>
+          <div className='mt-4'>
+            <button onClick={() => decrypt()} className='w-full bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded'>Decrypt</button>
+          </div>
+          <div className='mt-4'>
+            <p>Plain text : {plainState == null ? '' : <span className='bg-slate-500 px-1'>{plainState}</span>}</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
